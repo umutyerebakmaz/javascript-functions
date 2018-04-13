8115ffd9c44d949e93c875b6f1e08d3742a28ca5
Umut Yerebakmaz <umutyerebakmaz@users.noreply.github.com>
2018-04-13 16:33:33 +0300

    Create age-calculator.js

---
+++ age-calculator.js
@@ -0,0 +1,16 @@
+async function calculateAge (birthDate, otherDate) {
+	birthDate = new Date(birthDate);
+	otherDate = new Date(otherDate);
+	
+	let years = (otherDate.getFullYear() - birthDate.getFullYear());
+	if (otherDate.getMonth() < birthDate.getMonth() ||
+	otherDate.getMonth() == birthDate.getMonth() && otherDate.getDate() < birthDate.getDate()) {
+	years--;
+	}
+	
+	return years;
+	
+	}
+	
+	const age = calculateAge("10/12/1979", "12/04/2018"); // Format: MM/DD/YYYY
+	console.log(age);
