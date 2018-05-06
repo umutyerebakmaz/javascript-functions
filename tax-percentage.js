8cdce4aa760fc3ed8e9ccf49c7990bed1b0f2806
Umut Yerebakmaz <umutyerebakmaz@users.noreply.github.com>
2018-05-06 11:13:35 +0300

    Update tax-percentage.js

--- tax-percentage.js
+++ tax-percentage.js
@@ -1,2 +1,4 @@
+// Arrow functionlarda argument tek olursa parantez kullanmaya gerek yok. aynı şekilde function içerisinde single line code 
+// yazacaksanız yine cury braces kullanmanıza gerek olmadan shorthand yazabilirsiniz.
 const tax = a => a * 1.18;
 console.log(tax(500)); // 590
