152b48885497bae2dded9a545033ffacc514545d
Umut Yerebakmaz <umutyerebakmaz@users.noreply.github.com>
2018-05-31 13:38:25 +0300

    Update array-merge.js

--- array-merge.js
+++ array-merge.js
@@ -1,10 +1,10 @@
 const mergedArr = $scope.nodes.map(node => {
        const info = $scope.infos.find(info => info.node_id === node.desc);
-            if (info) {
-                return Object.assign({}, node, info);
-            } else {
-                return node;
-            }
-        });
+       if (info) {
+              return Object.assign({}, node, info);
+       } else {
+              return node;
+              }
+});
 
-        $scope.nodes = mergedArr;
+console.log(mergedArr);
