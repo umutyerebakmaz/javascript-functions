641073f82f71a2ce4e57543085c3fb706217dab6
Umut Yerebakmaz <umutyerebakmaz@users.noreply.github.com>
2018-06-12 10:26:18 +0300

    Create ramdom-string.js

---
+++ ramdom-string.js
@@ -0,0 +1,6 @@
+ //http://stackoverflow.com/questions/105034/how-to-create-a-guid-uuid-in-javascript
+ function randomString(){
+ return Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15); 
+ }
+
+ randomString();
