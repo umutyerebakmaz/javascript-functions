dfeb0a257ca815d3bcec43fbfd3363a2f8279e82
Umut Yerebakmaz <umutyerebakmaz@users.noreply.github.com>
2018-06-29 17:50:32 +0300

    Update pg-format-date.js

--- pg-format-date.js
+++ pg-format-date.js
@@ -1,16 +1,12 @@
 // Convert Javascript date to PostgreSQL TIMESTAMP YYYY-MM-DD HH:MI:SS:MS
 const datenow = new Date();
-
 function pgFormatDate(date) {
     function zeroPad(d) {
         return ('0' + d).slice(-2);
-    }
-    
+    } 
     const parsed = new Date(date);
-    
     return parsed.getUTCFullYear() +'-'+ zeroPad(parsed.getUTCMonth() + 1) +'-'+ zeroPad(parsed.getUTCDay()) +' '+
         zeroPad(parsed.getUTCHours()) +':'+ zeroPad(parsed.getUTCMinutes()) +':'+ zeroPad(parsed.getUTCSeconds()) +'.'+
         parsed.getMilliseconds();
 }
-
 console.log(pgFormatDate(datenow));
