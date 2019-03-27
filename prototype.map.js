86d622b4a31638702896a5d81f0fae305f89b70b
Umut Yerebakmaz <umutyerebakmaz@users.noreply.github.com>
2019-03-27 15:05:43 +0300

    Update prototype.map.js

--- prototype.map.js
+++ prototype.map.js
@@ -1,7 +1,7 @@
 const authors = [
-{id: "5c6a7eeb3e37f613084f22b2", name: "Future"},
-{id: "5c6a802d3e37f613084f22c9", name: "ASAP"},
-{id: "5c6a7f2c3e37f613084f22b6", name: "Lil Baby"},
+{id: "5c6a7eeb3e37f613084f22b2", name: "Adam Fewer"},
+{id: "5c6a802d3e37f613084f22c9", name: "Charles Bukowski"},
+{id: "5c6a7f2c3e37f613084f22b6", name: "Yaşar Kemal"},
 ];
 
 // pass a function to map
