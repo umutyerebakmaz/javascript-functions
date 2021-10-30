412d4f219ff882e725849a8eceff2e6a4095309f
umut yerebakmaz <umutyerebakmaz@users.noreply.github.com>
2021-10-30 22:04:33 +0300

    Update prototype.map.js

--- prototype.map.js
+++ prototype.map.js
@@ -4,8 +4,6 @@ const authors = [
 {id: "5c6a7f2c3e37f613084f22b6", name: "Yaşar Kemal"},
 ];
 
-// pass a function to map
-const authorIdList = authors.map(object => object.id);
+const authorIdList = authors.map(obj => obj.id);
 
 console.log(authorIdList);
-// expected output: Array ["5c6a7eeb3e37f613084f22b2", "5c6a802d3e37f613084f22c9", "5c6a7f2c3e37f613084f22b6"]
