7f8ec1addabe671e9bc3f6d6016ee190b2cbc599
umut yerebakmaz <umutyerebakmaz@users.noreply.github.com>
2021-10-30 21:25:35 +0300

    Update promise.js

--- promise.js
+++ promise.js
@@ -1,25 +1,37 @@
-let promise1 = new Promise(function(resolve, reject){
-    setTimeout(function(){
+let promise1 = new Promise(function (resolve, reject) {
+    setTimeout(function () {
         resolve('Resolved!');
     }, 1000);
 });
 
-let promise2 = new Promise(function(resolve, reject){
-    setTimeout(function(){
+let promise2 = new Promise(function (resolve, reject) {
+    setTimeout(function () {
         reject('Rejected!');
     }, 2000);
 });
 
-Promise.all([promise1,promise2])
-    .then(function(success){
-        console.log(success);
+/*
+* Promise.all() 
+* All promises are expected. 
+* If one of the promises is rejected, the result of rejected is returned.
+*/
+Promise.all([promise1, promise2])
+    .then((value) => {
+        console.log(value);
     })
-    .catch(function(error){
+    .catch((error) => {
         console.log(error);
     });
-
-/*
-* Promise.all, tüm promiseların cevabı beklenir. promiselardan birisi rejected ise rejected sonucu döner.
-* Promise.race, race te ise en hızlı promise hangisiyse onun sonucu döner. örnekte 1ms saniye sonra gerçekleşecek
-* promise sonucu için Resolved! sonucu dönecektir.
-* */
+/**
+ * Promise.race()
+ * Returns the fastest promise in race.
+ * in the example it will happen after 1ms seconds
+ * Resolved for promise result! will return the result.
+ */
+Promise.race([promise1, promise2])
+    .then((value) => {
+        console.log(value);
+    })
+    .catch((error) => {
+        console.log(error);
+    });
